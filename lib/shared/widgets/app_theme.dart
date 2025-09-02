48b2b016ba766a6509bbb55db16cbaf580f4faff
Aya Mohamed <ayahame99@gmail.com>
2025-09-02 04:31:16 +0300

    edit pubspec

--- lib/shared/widgets/app_theme.dart
+++ lib/shared/widgets/app_theme.dart
@@ -1,4 +1,20 @@
-class AppTheme {
-  
+import 'dart:ui';
 
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static const Color primary = Color(0xffF6BD00);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color blackSecondary = Color(0xff121312);
+  static const Color blackPrimary = Color(0xff000000);
+  static const Color grey = Color(0xff282A28);
+  static const Color red = Color(0xffE82626);
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: blackSecondary,
+    appBarTheme: AppBarTheme(
+      color: blackSecondary,
+      centerTitle: true,
+      foregroundColor: primary,
+    )
+  );
 }
