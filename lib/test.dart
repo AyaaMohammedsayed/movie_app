7e07c3574e994bd3a1d2c607a2ba193a3b893c0d
wateenmohamed <wateenmohamed41@gmail.com>
2025-09-02 17:46:10 +0300

    test

---
+++ lib/test.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movie_app/shared/widgets/app_theme.dart';
+
+class Test extends StatelessWidget {
+ 
+  @override
+  Widget build(BuildContext context) {
+
+ return Scaffold();
+ 
+   }
+}
