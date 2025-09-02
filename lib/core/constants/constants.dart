102bf8fff58df1a72254bf24169f72a8e16e93ce
mohamed_Elsayed <mh3010357@gmail.com>
2025-09-02 19:25:15 +0300

    implement login screen ui

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,24 @@
+class AppTexts {
+  static const loginSuccess = 'Login Successfully';
+  static const loginFailed = 'Invalid Email or Password';
+  static const mailName = 'E-mail';
+  static const passName = 'Password';
+  static const forgetPass = 'ForgetPassword?';
+  static const login = 'Login';
+  static const dntHaveAccount = "Don't Have Account ?";
+  static const createOne = 'Create One';
+  static const or = 'Or';
+  static const googleLogin = 'Login With Google';
+}
+
+class AppImages {
+  static const _imgFolder = 'assets/images';
+  static const _iconFolder = 'assets/icons';
+
+  static const logoImage = '$_imgFolder/icon_app.png';
+  static const mailIcon = '$_iconFolder/mail_icon.svg';
+  static const passIcon = '$_iconFolder/password_icon.svg';
+  static const googleIcon = '$_iconFolder/google_icon.svg';
+  static const enIcon = '$_iconFolder/en_icon.png';
+  static const egIcon = '$_iconFolder/eg_icon.png';
+}
---
+++ lib/features/auth/view/widgets/custom_circle_avatar.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:movie_app/core/app_theme.dart';
+
+class CustomCircleAvatar extends StatefulWidget {
+  final String iconName;
+  final VoidCallback onTap;
+  final bool tappedValue;
+
+  const CustomCircleAvatar({
+    required this.iconName,
+    required this.tappedValue,
+    required this.onTap,
+    super.key,
+  });
+
+  @override
+  State<CustomCircleAvatar> createState() => _CustomCircleAvatarState();
+}
+
+class _CustomCircleAvatarState extends State<CustomCircleAvatar> {
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: widget.onTap,
+      child: CircleAvatar(
+        backgroundColor: AppTheme.primary,
+        radius: widget.tappedValue == true ? 20 : 18,
+        child: CircleAvatar(
+          radius: 18,
+          backgroundImage: AssetImage(widget.iconName),
+        ),
+      ),
+    );
+  }
+}
