42d8aa9bbd1d2ae21bde989fb905d67ea2720bcd
wateenmohamed <wateenmohamed41@gmail.com>
2025-09-02 17:46:55 +0300

    add

--- lib/test.dart
+++ lib/test.dart
@@ -3,11 +3,8 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movie_app/shared/widgets/app_theme.dart';
 
 class Test extends StatelessWidget {
- 
   @override
   Widget build(BuildContext context) {
-
- return Scaffold();
- 
-   }
+    return Scaffold();
+  }
 }
