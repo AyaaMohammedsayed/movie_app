1fa280a110aff8508f6245f77809acf414c2ada7
Aya Mohamed <ayahame99@gmail.com>
2025-09-02 06:23:38 +0300

    initial file Update_profile

--- lib/profile/update_profile.dart
+++ lib/profile/update_profile.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class UpdateProfile extends StatelessWidget {
   static const String routeName = '/on_boarding';
-  const UpdateProfile({super.key});
+
 
   @override
   Widget build(BuildContext context) {
