495e4786a516f40d76dacb07b290440cecac973c
Rahma Sobih <rahmasobih25@gmail.com>
2025-09-02 17:40:50 +0300

    implement themes of app

--- lib/shared/widgets/app_theme.dart
+++ lib/shared/widgets/app_theme.dart
@@ -1,1 +1,65 @@
+import 'package:flutter/material.dart';
 
+class AppTheme {
+  static const Color black = Color(0xFF121312);
+  static const Color gray = Color(0xFF282A28);
+  static const Color yellow = Color(0xFFF6BD00);
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color green = Color(0xFF57AA53);
+  static const Color red = Color(0xFFE82626);
+
+  static ThemeData darkTheme = ThemeData(
+    scaffoldBackgroundColor: black,
+
+    appBarTheme: AppBarTheme(
+      backgroundColor: black,
+      foregroundColor: yellow,
+      centerTitle: true,
+      titleTextStyle: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w400,
+        color: yellow,
+      ),
+    ),
+
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: yellow,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: gray,
+      hintStyle: TextStyle(
+        fontSize: 16,
+        fontWeight: FontWeight.w500,
+        color: white,
+      ),
+      enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: gray),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: gray),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      errorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: red),
+        borderRadius: BorderRadius.circular(16),
+      ),
+      focusedErrorBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: red),
+        borderRadius: BorderRadius.circular(16),
+      ),
+    ),
+
+    textTheme: TextTheme(
+      titleMedium: TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.w400,
+        color: white,
+      ),
+    ),
+  );
+}
