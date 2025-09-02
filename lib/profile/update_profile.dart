1a39ad64be0fbfd19ffcdd8178926a78412d5a2a
Aya Mohamed <ayahame99@gmail.com>
2025-09-02 06:18:51 +0300

    edit_splash_size

---
+++ lib/profile/update_profile.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class UpdateProfile extends StatelessWidget {
+  static const String routeName = '/on_boarding';
+  const UpdateProfile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
