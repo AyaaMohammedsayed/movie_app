dd75d6edaee4aa24b136ff00f2b7661721010f60
Aya <ayahame99@gmail.com>
2025-09-24 14:06:41 +0300

    Modifiy Update UI to integrate with Logic

--- lib/features/tabs/profile_tab/presentation/screens/update_profile.dart
+++ lib/features/tabs/profile_tab/presentation/screens/update_profile.dart
@@ -8,7 +8,6 @@ import 'package:movie_app/core/widgets/custom_elevated_button.dart';
 import 'package:movie_app/core/app_theme.dart';
 import 'package:movie_app/core/widgets/custom_text_form_field.dart';
 import 'package:movie_app/features/auth/presentation/screens/login_screen.dart';
-import 'package:movie_app/features/tabs/profile_tab/data/model/delete_request.dart';
 import 'package:movie_app/features/tabs/profile_tab/data/model/reset_password_request.dart';
 import 'package:movie_app/features/tabs/profile_tab/data/model/update_request.dart';
 import 'package:movie_app/features/tabs/profile_tab/presentation/cubit/profile_cubit.dart';
@@ -30,14 +29,26 @@ class _UpdateProfileState extends State<UpdateProfile> {
   TextEditingController userPhone = TextEditingController();
   TextEditingController oldPasswordController = TextEditingController();
   TextEditingController newPasswordController = TextEditingController();
-
+  late AppLocalizations appLocalizations;
+  String email = '';
   int profile = 1;
-  int selectedItem = -1;
+  int selectedItem = 1;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    appLocalizations = AppLocalizations.of(context)!;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    context.read<ProfileCubit>().getProfile();
+  }
 
   @override
   Widget build(BuildContext context) {
     final textStyle = Theme.of(context).textTheme;
-    final appLocalizations = AppLocalizations.of(context)!;
 
     return Scaffold(
       appBar: AppBar(
@@ -45,56 +56,36 @@ class _UpdateProfileState extends State<UpdateProfile> {
       ),
       body: BlocConsumer<ProfileCubit, ProfileState>(
         listener: (context, state) {
-          // ===== Profile fetching =====
-          if (state is GetProfileLoading) UiUtils.showLoading(context);
-          if (state is GetProfileSuccess) {
-            UiUtils.hideLoading(context);
-            UiUtils.showSuccessMessage(state.response.message!);
-            userName.text = state.response.data!.name;
-            userPhone.text = state.response.data!.phone;
-            profile = state.response.data!.avaterId;
-          }
-          if (state is GetProfileError) {
-            UiUtils.hideLoading(context);
-            UiUtils.showErrorMessage(state.message);
-          }
-
-          // ===== Reset Password =====
-          if (state is ResetPasswordProfileLoading) UiUtils.showLoading(context);
-          if (state is ResetPasswordProfileSuccess) {
-            UiUtils.hideLoading(context);
-            UiUtils.showSuccessMessage(state.resetResponse.message!);
-          }
-          if (state is ResetPasswordProfileError) {
-            UiUtils.hideLoading(context);
-            UiUtils.showErrorMessage(state.message);
-          }
-
-          // ===== Delete Profile =====
-          if (state is DeleteProfileLoading) UiUtils.showLoading(context);
+    
           if (state is DeleteProfileSuccess) {
-            UiUtils.hideLoading(context);
             UiUtils.showSuccessMessage(state.deleteResponse.message!);
-            Navigator.of(context).pushNamedAndRemoveUntil(LoginScreen.routeName, (route) => false);
+            Navigator.of(
+              context,
+            ).pushNamedAndRemoveUntil(LoginScreen.routeName, (route) => false);
           }
-          if (state is DeleteProfileError) {
-            UiUtils.hideLoading(context);
-            UiUtils.showErrorMessage(state.message);
-          }
-
-          // ===== Update Profile =====
-          if (state is UpdateProfileLoading) UiUtils.showLoading(context);
           if (state is UpdateProfileSuccess) {
-            UiUtils.hideLoading(context);
             UiUtils.showSuccessMessage(state.updateResponse.message!);
             Navigator.of(context).pop();
+            context.read<ProfileCubit>().getProfile();
           }
-          if (state is UpdateProfileError) {
-            UiUtils.hideLoading(context);
-            UiUtils.showErrorMessage(state.message);
+          if (state is ResetPasswordProfileSuccess) {
+            UiUtils.showSuccessMessage(state.resetResponse.message!);
+          }
+          if (state is DeleteProfileError ||
+              state is UpdateProfileError ||
+              state is ResetPasswordProfileError) {
+            UiUtils.showErrorMessage((state as dynamic).message);
           }
         },
         builder: (context, state) {
+        
+          if (state is GetProfileSuccess) {
+            userName.text = state.response.data!.name;
+            userPhone.text = state.response.data!.phone ?? "";
+            profile = state.response.data!.avaterId;
+            email = state.response.data!.email;
+          }
+         
           return Padding(
             padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 37.h),
             child: SingleChildScrollView(
@@ -104,7 +95,7 @@ class _UpdateProfileState extends State<UpdateProfile> {
                   InkWell(
                     onTap: () => _showAvatarSheet(context),
                     child: Image.asset(
-                      'assets/images/avatar$profile.png',
+                      'assets/images/avatar$selectedItem.png',
                       width: 150.w,
                       height: 150.h,
                       fit: BoxFit.fill,
@@ -112,55 +103,69 @@ class _UpdateProfileState extends State<UpdateProfile> {
                     ),
                   ),
                   SizedBox(height: 35.h),
-
-                  // ===== UserName =====
+ 
                   TextField(
                     controller: userName,
-                    style: TextStyle(fontSize: 20.sp, color: const Color.fromARGB(255, 134, 12, 12)),
+                    style: TextStyle(fontSize: 20.sp, color: AppTheme.white),
                     decoration: InputDecoration(
-                      prefixIcon: Image.asset(AppImages.userImage, width: 30.w, height: 30.h, fit: BoxFit.scaleDown),
+                      prefixIcon: Image.asset(
+                        AppImages.userImage,
+                        width: 30.w,
+                        height: 30.h,
+                        fit: BoxFit.scaleDown,
+                      ),
                     ),
                   ),
                   SizedBox(height: 20.h),
-
                   // ===== Phone =====
                   TextField(
                     controller: userPhone,
                     style: TextStyle(fontSize: 20.sp, color: AppTheme.white),
                     decoration: InputDecoration(
-                      prefixIcon: Image.asset(AppImages.phoneImage, width: 30.w, height: 30.h, fit: BoxFit.scaleDown),
+                      prefixIcon: Image.asset(
+                        AppImages.phoneImage,
+                        width: 30.w,
+                        height: 30.h,
+                        fit: BoxFit.scaleDown,
+                      ),
                     ),
                   ),
                   SizedBox(height: 30.h),
-
                   // ===== Reset Password =====
                   Align(
                     alignment: Alignment.centerLeft,
                     child: TextButton(
                       onPressed: () => _showResetPasswordBottomSheet(context),
-                      child: Text(appLocalizations.restPass, style: textStyle.titleLarge),
+                      child: Text(
+                        appLocalizations.restPass,
+                        style: textStyle.titleLarge,
+                      ),
                     ),
                   ),
-
                   SizedBox(height: MediaQuery.sizeOf(context).height * 0.25.h),
-
                   // ===== Delete Account =====
                   CustomElevatedButton(
                     foregroundColor: AppTheme.white,
-                    onTap: () => context.read<ProfileCubit>().deleteProfile(DeleteRequest(name: userName.text)),
+                    onTap: () => context.read<ProfileCubit>().deleteProfile(),
                     backgroundColor: AppTheme.red,
                     child: Text(appLocalizations.deleteAccount),
                   ),
                   SizedBox(height: 19.h),
-
                   // ===== Update Account =====
                   CustomElevatedButton(
                     child: Text(appLocalizations.updataAccount),
-                    onTap: () => context.read<ProfileCubit>().updateProfile(UpdateRequest(
-                      name: userName.text,
-                      phone: userPhone.text,
-                      avaterId: profile.toString(),
-                    )),
+                    onTap: () {
+                      final updateRequest = UpdateRequest(
+                        email:
+                            email.isNotEmpty
+                                ? email
+                                : '${userName.text}@gmail.com',
+                        name: userName.text.isNotEmpty ? userName.text : '',
+                        phone: userPhone.text.isNotEmpty ? userPhone.text : '',
+                        avaterId: selectedItem.toString(), 
+                      );
+                      context.read<ProfileCubit>().updateProfile(updateRequest);
+                    },
                   ),
                 ],
               ),
@@ -174,71 +179,82 @@ class _UpdateProfileState extends State<UpdateProfile> {
   void _showAvatarSheet(BuildContext context) {
     showModalBottomSheet(
       context: context,
+      isScrollControlled: true,
       backgroundColor: Colors.transparent,
-      builder: (context) => ModalBottomSheet(
-        selectedItem: selectedItem,
-        onSelect: (newProfile, newIndex) {
-          setState(() {
-            profile = newProfile;
-            selectedItem = newIndex;
-          });
-        },
-      ),
+      builder:
+          (context) => ModalBottomSheet(
+            selectedItem: profile,
+            onSelect: (newProfile, newIndex) {
+              setState(() {
+                selectedItem = newProfile;
+                profile = selectedItem;
+              });
+            },
+          ),
     );
   }
 
   void _showResetPasswordBottomSheet(BuildContext context) {
-    final appLocalizations = AppLocalizations.of(context)!;
     final textStyle = Theme.of(context).textTheme;
 
     showModalBottomSheet(
       context: context,
-      isScrollControlled: true,
+      isScrollControlled: true, 
       backgroundColor: Colors.transparent,
       builder: (context) {
-        return Container(
-          padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 20.h),
-          decoration: BoxDecoration(
-            color: AppTheme.grey,
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
+        return Padding(
+          padding: EdgeInsets.only(
+            left: 16.w,
+            right: 16.w,
+            top: 20.h,
+            bottom:
+                MediaQuery.of(context).viewInsets.bottom +
+                20.h,
           ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(appLocalizations.restPass, style: textStyle.titleLarge),
-              SizedBox(height: 20.h),
-
-              CustomTextFormField(
-                hintText: appLocalizations.passName,
-                controller: oldPasswordController,
-                validator: Validator.validatePassword,
-                prefixIconName: AppImages.passIcon,
-                isPassword: true,
+          child: Container(
+            decoration: BoxDecoration(
+              color: AppTheme.grey,
+              borderRadius: BorderRadius.vertical(top: Radius.circular(20.r)),
+            ),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  SizedBox(height: 20.h),
+                  Text(appLocalizations.restPass, style: textStyle.titleLarge),
+                  SizedBox(height: 20.h),
+                  CustomTextFormField(
+                    hintText: appLocalizations.passName,
+                    controller: oldPasswordController,
+                    validator: Validator.validatePassword,
+                    prefixIconName: AppImages.passIcon,
+                    isPassword: true,
+                  ),
+                  SizedBox(height: 20.h),
+                  CustomTextFormField(
+                    hintText: appLocalizations.passName,
+                    controller: newPasswordController,
+                    validator: Validator.validatePassword,
+                    prefixIconName: AppImages.passIcon,
+                    isPassword: true,
+                  ),
+                  SizedBox(height: 25.h),
+                  CustomElevatedButton(
+                    child: Text(appLocalizations.restPass),
+                    onTap: () async {
+                      await context.read<ProfileCubit>().resetPassword(
+                        ResetPasswordRequest(
+                          oldPassword: oldPasswordController.text,
+                          newPassword: newPasswordController.text,
+                        ),
+                      );
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  SizedBox(height: 20.h),
+                ],
               ),
-              SizedBox(height: 20.h),
-
-              CustomTextFormField(
-                hintText: appLocalizations.passName,
-                controller: newPasswordController,
-                validator: Validator.validatePassword,
-                prefixIconName: AppImages.passIcon,
-                isPassword: true,
-              ),
-              SizedBox(height: 25.h),
-
-              CustomElevatedButton(
-                child: Text(appLocalizations.restPass),
-                onTap: () async {
-                  final cubit = context.read<ProfileCubit>();
-                  await cubit.resetPassword(ResetPasswordRequest(
-                    oldPassword: oldPasswordController.text,
-                    newPassword: newPasswordController.text,
-                  ));
-                  Navigator.of(context).pop();
-                },
-              ),
-              SizedBox(height: 20.h),
-            ],
+            ),
           ),
         );
       },
