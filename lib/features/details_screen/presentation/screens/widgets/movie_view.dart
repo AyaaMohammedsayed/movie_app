7c182bbbfcfb791406eb26550e0e77b8c233ec80
Aya <ayahame99@gmail.com>
2025-09-24 22:48:46 +0300

    done

--- lib/features/details_screen/presentation/screens/widgets/movie_view.dart
+++ lib/features/details_screen/presentation/screens/widgets/movie_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:movie_app/core/app_theme.dart';
 import 'package:movie_app/core/utils/localize_app_localization.dart';
